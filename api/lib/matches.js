00a0ea5a643aeb8924247a30bab12e5e940fc6da
Alex Henderson <alex@alexhenderson.me>
2019-02-06 18:57:11 -0800

    Initial implementation of matches/summoner api

---
+++ api/lib/matches.js
@@ -0,0 +1,76 @@
+const axios = require('axios');
+
+const apiKey = process.env.LOL_API_KEY;
+
+const getMatchByGameId = async (gameId, region = 'NA1') => {
+  const response = await axios.get(
+    `https://${region}.api.riotgames.com/lol/match/v4/matches/${matchId}?api_key=${apiKey}`
+  );
+
+  return response.data;
+};
+
+const getMatchSummary = async (match, perspective) => {
+  const {accountId: perspectiveAccountId} = perspective;
+
+  const participants = await Promise.all(
+    match.participants.map(async p => ({
+      participantId: p.participantId,
+      team: p.teamId === 100 ? 'blue' : 'red',
+      spells: [await getSpell(p.spell1Id), await getSpell(p.spell2Id)],
+      champion: getChampion(p.championId),
+      level: p.stats.champLevel,
+      kills: p.stats.kills,
+      assists: p.stats.assits,
+      deaths: p.stats.deaths,
+      wards: p.stats.wardsPlaced,
+      items: [
+        // Array here doesn't preserve item spacing
+        await getItem(p.stats.item0),
+        await getItem(p.stats.item1),
+        await getItem(p.stats.item2),
+        await getItem(p.stats.item3),
+        await getItem(p.stats.item4),
+        await getItem(p.stats.item5),
+        await getItem(p.stats.item6),
+      ],
+      creepScore: p.stats.neutralMinionsKilled,
+      masteries: {
+        // TODO: (maybe?) - get details of masteries
+        // this should be enough for displaying images though
+        keystone: p.stats.perk0,
+        primary: [p.stats.perk1, p.stats.perk2, p.stats.perk3],
+        secondary: [p.stats.perk4, p.stats.perk5],
+      },
+    }))
+  );
+
+  const players = match.participantIdentities.map(identity => ({
+    data: participants.find(p => p.participantId == identity.participantId),
+    accountId: identity.player.currentAccountId,
+    name: p.player.summonerName,
+    profileIcon: p.player.profileIcon,
+  }));
+
+  const teams = match.teams.map(t => ({
+    color: t.teamId === 100 ? 'blue' : 'red',
+    victory: t.win === 'Win' ? true : false,
+    players: players.filter(
+      p => p.data.team === (t.teamId === 100 ? 'blue' : 'red')
+    ),
+  }));
+
+  return {
+    gameId: match.gameId,
+    duration: match.gameDuration,
+    teams,
+    players,
+    perspective:
+      players.find(p => p.accountId === perspectiveAccountId) || null,
+  };
+};
+
+module.exports = {
+  getMatchByGameId,
+  getMatchSummary,
+};
---
+++ api/lib/summoners.js
@@ -0,0 +1,23 @@
+const axios = require('axios');
+
+const apiKey = process.env.LOL_API_KEY;
+
+const getSummonerByName = async (name, region = 'NA1') => {
+  const response = await axios.get(
+    `https://${region}.api.riotgames.com/lol/summoner/v4/summoners/by-name/${name}?api_key=${apiKey}`
+  );
+  return response.data;
+};
+
+const getSummonerMatches = async (summoner, region = 'NA1') => {
+  const {accountId} = summoner;
+  const response = await axios.get(
+    `https://${region}.api.riotgames.com/lol/match/v4/matchlists/by-account/${accountId}?api_key=${apiKey}`
+  );
+  return response.data;
+};
+
+module.exports = {
+  getSummonerByName,
+  getSummonerMatches,
+};
