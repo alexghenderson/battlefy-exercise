c5f50c5c876ea5f0c81b9c25945bc7dc4b024019
Alex Henderson <alex@alexhenderson.me>
2019-02-06 20:23:31 -0800

    Created hook for input fields

---
+++ client/lib/use-field.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+function useField(defaultValue) {
+  const [value, setValue] = React.useState(defaultValue);
+
+  const onChange = useCallback(e => {
+    if (typeof e === 'object' && 'target' in e && 'value' in e.target) {
+      // Treat e as an event
+      setValue(e.target.value);
+    } else {
+      // Treat e as a raw value
+      setValue(e);
+    }
+  }, []);
+
+  return {
+    field: {
+      value,
+      onChange
+    }
+  };
+}
