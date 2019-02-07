be8feee268e37a031982bbef4682a0df20806ba9
Alex Henderson <alex@alexhenderson.me>
2019-02-06 19:31:07 -0800

    Add route detection to dev server

--- dev-server.js
+++ dev-server.js
@@ -1,5 +1,20 @@
 require('dotenv').config();
+const config = require('./now.json');
+
+const routes = config.routes.reduce(
+  (r, route) => ({
+    ...r,
+    [route.src]: require(`./${route.dest}`),
+  }),
+  {}
+);
+
+const notFound = (_, res) => {
+  res.statusCode = 404;
+  res.end('Not found');
+};
 
 module.exports = async (req, res) => {
-  res.end();
-}
+  const [url] = req.url.split('?');
+  return (routes[url] || notFound)(req, res);
+};
